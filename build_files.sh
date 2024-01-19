ad2ad0c22346d681bbfbdfccb4e669b86772617d
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2024-01-19 13:49:20 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-pip install -r requirments.txt
-pip install pytz
-pip install django
+pip install pytz asgiref attrs autobahn Automat cffi channels constantly cryptography daphne django django-ckeditor django-js-asset hyperlink idna incremental psycopg psycopg2 psycopg2-binary pyasn1 pyasn1-modules pycparser pyOpenSSL pytz service-identity setuptools six sqlparse Twisted twisted-iocpsupport txaio typing_extensions tzdata zope.interface
+
 python3 manage.py collectstatic
