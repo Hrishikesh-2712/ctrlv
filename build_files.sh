eca6fb7bcf5a72c44aea41ee094ef56c032cb2c1
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2024-01-19 13:53:49 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,2 @@
-pip install pytz asgiref attrs autobahn Automat cffi channels constantly cryptography daphne django django-ckeditor django-js-asset hyperlink idna incremental psycopg2-binary pyasn1 pyasn1-modules pycparser pyOpenSSL pytz service-identity setuptools six sqlparse Twisted twisted-iocpsupport txaio typing_extensions tzdata zope.interface
-
+pip3 install -r requirments.txt
 python3 manage.py collectstatic
